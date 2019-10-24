8b3a72e4e65d37cb749656e51925810ea6eadd90
jswang <jswang416@gmail.com>
2019-10-24 11:32:58 +0800

    docs(mymath): Fix comments in 2 functions

--- mymath/add.go
+++ mymath/add.go
@@ -1,13 +1,13 @@
 package mymath
 
 // Add adds the input integer.
-// BUG(mepaling): can only used in "int" type
+// BUG(mepaling): can only used in "int" type.
 func Add(a int, b int) int {
 	return a + b
 }
 
 // Sub subs the input integer.
-// BUG(mepaling): can only used in "int" type
+// BUG(mepaling): can only used in "int" type.
 func Sub(a int, b int) int {
 	return a - b
 }
