00818699c903c4f471fbb9fda8ccd16337c8cfc5
jswang <jswang416@gmail.com>
2019-10-24 11:09:02 +0800

    feat(mymath): Add Sub function

--- mymath/add.go
+++ mymath/add.go
@@ -5,3 +5,9 @@ package mymath
 func Add(a int, b int) int {
 	return a + b
 }
+
+// Sub subs the input integer.
+// BUG(mepaling): can only used in "int" type
+func Sub(a int, b int) int {
+	return a - b
+}
